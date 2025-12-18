3e9d8f3985696b7fa9190bcca70dde2dd24b494e
Syam Sasi <syamsasi99@gmail.com>
2025-12-19 02:44:47 +0800

    Update docker_push.sh

--- docker_push.sh
+++ docker_push.sh
@@ -3,7 +3,7 @@
 set -e  # Exit if any command fails
 
 # Define variables
-IMAGE_NAME="taqelah/playpi_dashboard"
+IMAGE_NAME="abhijeetvaikar/playpi"
 TAG="latest"
 
 echo "Building Docker image for PlayPI Dashboard..."
